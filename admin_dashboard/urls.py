533de1ec92a095c602b6cb728df9b0cef85c20dd
omengoemmanuel <omengomanuel@gmail.com>
2025-04-04 19:11:19 +0300

    new

--- admin_dashboard/urls.py
+++ admin_dashboard/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('admin', views.admin_login, name='admin_login'),
     path('logout_user', views.logout_user, name='logout_user'),
-    path('admin_dashboard', views.admin_board, name='admin_board'),
+    path('admin_board', views.admin_board, name='admin_board'),
     path('layout', views.layout, name='layout'),
     path('profile', views.profile, name='profile'),
     path('settings', views.settings, name='settings'),
